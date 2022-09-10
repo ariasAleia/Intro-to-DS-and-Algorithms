2399838468320e47bda48a93b899869abe016f7c
ariasAleia <ariasfaleja@gmail.com>
2022-09-10 13:37:29 -0500

    Update function to work with lists
    We can change with the same index

--- scripts/swap.py
+++ scripts/swap.py
@@ -3,6 +3,14 @@ def swap_elements(input_list, x, y):
     Swaps the value of the element x and element y
     using no more variables! Just a hack I like :)
     """
-    input_list[x] = input_list[x] - input_list[y]
-    input_list[y] = input_list[x] + input_list[y]
-    input_list[x] = input_list[y] - input_list[x]
+    valX = input_list[x]
+    valY = input_list[y]
+    
+    valX = valX - valY
+    valY = valX + valY
+    valX = valY - valX
+    
+    #We had to separate the procedure because we are working with lists
+    
+    input_list[x] = valX
+    input_list[y] = valY
